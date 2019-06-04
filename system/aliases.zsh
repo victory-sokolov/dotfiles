026b324126af947158679038ee5e312ac2622767
victory-sokolov <viktorsokolov.and@gmail.com>
2019-06-04 11:01:37 +0300

    zsh aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -4,6 +4,8 @@ alias update="sudo apt-get update && sudo apt-get upgrade"
 alias install="sudo apt-get install"
 alias remove="sudo apt-get remove"
 
+# Print each PATH entry on a separate line
+alias path="echo -e ${PATH//:/\\n}"
 
 # Directory traversal
 alias ..="cd .."
@@ -14,22 +16,33 @@ alias -- -="cd -"
 alias go="cd "$@" && ls"
 
 alias cls="clear"
-alias grep='grep --color=auto '
+alias grep="grep --color=auto "
+
+alias q="exit"
+
+# Empty trash.
+alias empty-trash="rm -rf ~/.local/share/Trash/files/*"
 
 # copy file interactive
-alias cp='cp -i'
+alias cp="cp -i"
 
 # move file interactive
-alias mv='mv -i'
+alias mv="mv -i"
+
+# remove file interactive
+alias rm='rm -i -v'
+
+alias time="date +%T"
+alias date="date +%D"
 
 # untar
-alias untar='tar xvf'
+alias untar="tar xvf"
 
-alias db='cd ~/Dropbox'
-alias dev='cd /home/viktor/Dropbox/Code/Code Files/'
+alias db="cd ~/Dropbox"
+alias dev="cd /home/viktor/Dropbox/Code/Code Files/"
 
 # vhosts
-alias hosts='sudo vim /etc/hosts'
+alias hosts="sudo vim /etc/hosts"
 
 # check for updates
 alias update="sudo apt-get update && sudo apt-get upgrade && sudo apt autoremove -y"
@@ -41,7 +54,7 @@ alias bashrc="vim ~/.bashrc"
 alias vimrc="vim ~/.vimrc"
 
 # quick reload
-alias reload_zsh='source ~/.zprofile'
+alias reload="source ~/.zprofile"
 
 #Youtube dll
 alias ytmp3="youtube-dl --extract-audio --audio-format mp3"
