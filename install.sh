26d29117237f7777ef811e431bb5e0879d65eea1
victory-sokolov <viktorsokolov.and@gmail.com>
2019-07-13 16:20:12 +0300

    added docker

--- install.sh
+++ install.sh
@@ -107,6 +107,17 @@ installation() {
 	make
 	sudo make install
 
+	# Docker
+	sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
+  sudo apt update
+  sudo apt install docker-ce -y
+  # execute docker without sudo
+  sudo usermod -aG docker ${USER}
+  su - ${USER}
+  id -nG
+
 }
 
 
