0807e97c0119ee0dc5fe609b15841cad25bb9ade
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-02-02 21:35:19 +0200

    Docker compose installation

--- install.sh
+++ install.sh
@@ -120,6 +120,10 @@ installation() {
 	sudo usermod -aG docker ${USER}
 	su - ${USER}
 	id -nG
+	# Docker compose
+	sudo curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	sudo chmod +x /usr/local/bin/docker-compose
+
 
 	# YouTube-DLL
 	sudo wget http://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
