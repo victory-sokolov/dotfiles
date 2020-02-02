c5071406a7d21b346cfbf29d0e8ef41030d9ff88
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-02-02 21:44:35 +0200

    added Docker Machine

--- install.sh
+++ install.sh
@@ -120,10 +120,16 @@ installation() {
 	sudo usermod -aG docker ${USER}
 	su - ${USER}
 	id -nG
+	
 	# Docker compose
 	sudo curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 	sudo chmod +x /usr/local/bin/docker-compose
-
+	
+	# Docker Machine
+	base=https://github.com/docker/machine/releases/download/v0.16.0
+	curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine
+	sudo mv /tmp/docker-machine /usr/local/bin/docker-machine
+	sudo chmod +x /usr/local/bin/docker-machine
 
 	# YouTube-DLL
 	sudo wget http://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
