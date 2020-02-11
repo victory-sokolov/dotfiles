8ece4891ed48b52640ccb2da2b314ae40b2c7b27
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-02-11 11:47:56 +0200

    removed watchman

--- install.sh
+++ install.sh
@@ -105,16 +105,6 @@ installation() {
 	sudo dpkg --install rescuetime.deb
 	rm rescuetime.deb
 
-	# Install watchman
-	# https://facebook.github.io/watchman/docs/install.html
-	sudo apt-get install libtool -y	
-	cd ~ && git clone https://github.com/facebook/watchman.git
-	cd ~/watchman
-	git checkout v4.9.0 # the latest stable release
-	sudo ./autogen.sh
-	./configure
-	make
-	sudo make install
 
 	# Docker
 	sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
