8e69e2794652e8f32a4565505dbcecc9780be6c4
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-02-11 11:34:32 +0200

    updated node packages & use npm without sudo

---
+++ node/node.zsh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Node install
+sudo npm cache clean -f
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo npm install npm@latest -g
+
+# Install npm packages globally without sudo on macOS and Linux
+mkdir "${HOME}/.npm-packages"
+npm config set prefix "${HOME}/.npm-packages"
+
+
+npm_scripts=(
+	"-g trash-cli",
+	"-g create-react-app",
+	"-g electron --save-dev --save-exact",
+	"-g gulp-cli",
+	"-g browser-sync",
+	"-g express",
+	"-g eslint",
+	"-g uncss" #remove unused css
+	"-g stylelint --save-dev"
+	"-g python-shell"
+	"-g vtop"
+	"-g localtunnel"
+	"-g typescript"
+	"-g gitbook"
+	"-g webpack"
+	"-g pm2"
+	)
+
+cd $HOME
+
+for element in "${npm_scripts[@]}"
+do
+    npm install ${element}
+done
+
