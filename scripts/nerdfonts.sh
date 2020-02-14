649df74145912283102377037d10c4788010c4bd
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-02-14 10:01:28 +0200

    updated

--- scripts/nerdfonts.sh
+++ scripts/nerdfonts.sh
@@ -2,7 +2,8 @@
 
 # install nerd fonts: https://github.com/ryanoasis/nerd-fonts#option-3-install-script
 
-git clone https://github.com/ryanoasis/nerd-fonts ~/.fonts
+cd ~/.fonts
+git clone https://github.com/ryanoasis/nerd-fonts 
 cd nerd-fonts
 ./install.sh
 fc-cache -fv
