71a9986560c6204a02ef7c9949ce8154304d7e36
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-03-24 18:18:56 +0200

    install java and android-sdk

--- scripts/android.sh
+++ scripts/android.sh
@@ -1,7 +1,10 @@
 #!bin/sh
 
 sudo snap install android-studio
-sudo apt install android-tools
 
+sudo apt install -y \
+    android-tools \
+    android-sdk \
+    default-jdk
 
 sudo apt-get install adb
