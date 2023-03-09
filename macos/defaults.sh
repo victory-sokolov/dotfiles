5517a5d7f870f6c05492a3b6d634f9dc1ccf1b7f
Viktor Sokolov <viktorsokolov.and@gmail.com>
2023-03-09 18:47:51 +0200

    add mac configurations

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -1,4 +1,4 @@
-
+SCREENSHOTS_FOLDER="${HOME}/Screenshots"
 
 # Move a window by clicking on any part of it 
 defaults write -g NSWindowShouldDragOnGesture -bool true
@@ -16,6 +16,8 @@ defaults write com.apple.finder ShowPathbar -bool true
 # Show the full URL in the address bar (note: this still hides the scheme)
 defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
 
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
 
 # Enable full keyboard access for all controls
 # (e.g. enable Tab in modal dialogs)
@@ -25,6 +27,8 @@ defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 defaults write NSGlobalDomain KeyRepeat -int 1
 defaults write NSGlobalDomain InitialKeyRepeat -int 10
 
+# Automatically illuminate built-in MacBook keyboard in low light
+defaults write com.apple.BezelServices kDim -bool true
 
 # Disable machine sleep while charging
 sudo pmset -c sleep 0
@@ -36,3 +40,50 @@ defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 # Don’t show recent applications in Dock
 defaults write com.apple.dock show-recents -bool false
 
+# Save screenshots to the ~/Screenshots folder
+mkdir -p "${SCREENSHOTS_FOLDER}"
+defaults write com.apple.screencapture location -string "${SCREENSHOTS_FOLDER}"
+
+# Save screenshots in PNG format
+defaults write com.apple.screencapture type -string "png"
+
+# Disable shadow in screenshots
+defaults write com.apple.screencapture disable-shadow -bool true
+
+# No bouncing icons
+defaults write com.apple.dock no-bouncing -bool true
+
+###############################################################################
+# Mail                                                                        #
+###############################################################################
+
+# Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app
+defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+
+# Disable sound for incoming mail
+defaults write com.apple.mail MailSound -string ""
+
+# Disable sound for other mail actions
+defaults write com.apple.mail PlayMailSounds -bool false
+
+# Disable includings results from trash in search
+defaults write com.apple.mail IndexTrash -bool false
+
+###############################################################################
+# Software Updates                                                            #
+###############################################################################
+
+# Enable the automatic update check
+defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
+
+# Check for software updates weekly (`dot update` includes software updates)
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -string 7
+
+# Download newly available updates in background
+defaults write com.apple.SoftwareUpdate AutomaticDownload -bool true
+
+# Install System data files & security updates
+defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -bool true
+
+# Turn on app auto-update
+defaults write com.apple.commerce AutoUpdate -bool true
