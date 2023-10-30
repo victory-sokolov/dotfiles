75a90487fdce8e7fadf9c91b5599a93918016868
Viktor Sokolov <viktorsokolov.and@gmail.com>
2023-10-30 21:42:52 +0200

    winget package installation

---
+++ windows/winget-install.ps1
@@ -0,0 +1,26 @@
+# Browser
+winget install Google.Chrome.Beta
+winget install Mozilla.Firefox.DeveloperEdition
+
+# Chat
+winget install Discord.Discord.Canary
+
+# Windows and CLI
+winget install Microsoft.PowerToys
+winget install Microsoft.WindowsTerminal.Preview
+winget install Microsoft.PowerShell.Preview
+winget install JanDeDobbeleer.OhMyPosh
+
+# Git
+winget install Git.Git
+winget install GitHub.cli
+
+# Development Enviroment
+winget install Microsoft.dotnet
+winget install Yarn.Yarn
+
+# IDE
+winget install Microsoft.VisualStudioCode
+
+# Miscellaneous
+winget install 7zip.7zip
